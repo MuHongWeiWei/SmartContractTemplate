0205254fa86bfc28042494a09bff4e99d02241f4
Wade <tony91097@gmail.com>
2022-03-31 17:11:02 +0800

    update

--- ERC721/RedCatCreate.sol
+++ ERC721/RedCatCreate.sol
@@ -5,10 +5,19 @@ import './RedCatTemplate.sol';
 
 contract RedCatCreate {
 
+    mapping(address => address[]) contracts;
+
     event ContractAddress(address createAddress, address contractAddress);
 
     function createContract(string memory name, string memory symbol, uint _maxMint, uint _porfit, uint _maxTotal, uint _price, uint _mintTime, string memory _baseTokenURI) public {
         address contractAddress = address(new RedCatTemplate(name, symbol, _maxMint, _porfit, _maxTotal, _price, _mintTime, _baseTokenURI));
+        
+        contracts[tx.origin].push(contractAddress);
+
         emit ContractAddress(tx.origin, contractAddress);
+    }
+
+    function getContractAddress(address _owner) public view returns (address[] memory) {
+        return contracts[_owner];
     }
 }
